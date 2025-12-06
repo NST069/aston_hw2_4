2c5d51433b5d181ffa9e98d781bcdee20e3e6493
NiKryukov <nislav.ns@gmail.com>
2025-12-07 01:16:58 +0300

    Swagger и HATEOAS для документации и навигации

--- notification-service/src/main/java/com/aston/notification_service/util/Result.java
+++ notification-service/src/main/java/com/aston/notification_service/util/Result.java
@@ -1,23 +1,24 @@
 package com.aston.notification_service.util;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Result {
+@Schema(description = "Стандартный ответ API")
+public class Result extends RepresentationModel<Result> {
 
-    @Getter @Setter
+    @Schema(description = "Флаг успешности операции", example = "true")
     private boolean isSuccessful;
 
-    @Getter @Setter
+    @Schema(description = "Код статуса операции", example = "200")
     private int statusCode;
 
-    @Getter @Setter
+    @Schema(description = "Сообщение о статусе операции", example = "Пользователь найден успешно")
     private String message;
 
-    @Getter @Setter
+    @Schema(description = "Ответное сообщение")
     private Object data;
 }
